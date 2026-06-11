8d86579501eec8f14875ffc4ce376dd393b4c11e
Jared Bruni <jaredbruni@gmail.com>
2026-06-11 11:02:17 -0700

    testapps shell script

---
+++ testapps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./run.pl hello_world
+./run.pl text_example
+./run.pl sprite_example
+./run.pl static_example
+./run.pl console_demo
+./run.pl fractal_zoom
+./run.pl model_example
+./run.pl asteroids
+./run.pl glitch_cube
+./run.pl tictactoe
+./run.pl pong
+./run.pl pool_demo
+./run.pl puzzle
+./run.pl tux_example
+./run.pl walk
