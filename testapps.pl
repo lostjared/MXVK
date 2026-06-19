ae1a3ee4aabc13f93e9bb0660e1e3725d080ff29
Jared Bruni <jaredbruni@gmail.com>
2026-06-19 11:34:17 -0700

    updated testapps to work to abort on SIGINT

--- testapps.pl
+++ testapps.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use POSIX qw(:sys_wait_h);
 
 my @tests = qw(
     hello_world
@@ -28,6 +29,8 @@ my @tests = qw(
 );
 
 my $count = 0;
+my $interrupted = 0;
+my $child_pid;
 
 sub shell_quote {
     my ($value) = @_;
@@ -36,13 +39,55 @@ sub shell_quote {
     return "'$value'";
 }
 
+local $SIG{INT} = sub {
+    $interrupted = 1;
+    kill 'INT', $child_pid if defined $child_pid;
+};
+
 for my $test (@tests) {
+    last if $interrupted;
+
     $count++;
     my @cmd = ('./run.pl', $test, @ARGV);
     print '>> Executing: ';
     print join(' ', map { shell_quote($_) } @cmd);
     print "\n";
-    my $rc = system(@cmd);
+    my $pid = fork();
+    die "Error: could not fork for program '$test': $!\n" if !defined $pid;
+
+    if ($pid == 0) {
+        exec @cmd;
+        die "Error: failed to exec program '$test': $!\n";
+    }
+
+    $child_pid = $pid;
+
+    my $rc;
+    while (1) {
+        last if $interrupted;
+
+        my $wait = waitpid($pid, WNOHANG);
+        if ($wait == $pid) {
+            $rc = $?; 
+            last;
+        }
+
+        if ($wait == -1) {
+            die "Error: failed waiting for program '$test': $!\n";
+        }
+
+        last if $interrupted;
+        select undef, undef, undef, 0.1;
+    }
+
+    if ($interrupted) {
+        waitpid($pid, 0);
+        last;
+    }
+
+    $child_pid = undef;
+
+    last if $interrupted;
 
     if ($rc == -1) {
         die "Error: program '$test' failed to start: $!\n";
@@ -50,6 +95,10 @@ for my $test (@tests) {
 
     if ($rc & 127) {
         my $signal = $rc & 127;
+        if ($signal == 2) {
+            $interrupted = 1;
+            last;
+        }
         die "Error: program '$test' failed on test $count: terminated by signal $signal\n";
     }
 
@@ -57,6 +106,11 @@ for my $test (@tests) {
     if ($exit_code != 0) {
         die "Error: program '$test' failed on test $count: exit code $exit_code\n";
     }
+}
+
+if ($interrupted) {
+    print STDERR "Interrupted by SIGINT.\n";
+    exit 130;
 }
 
 print "$count tests ran and they were all successful.\n";
--- testapps.sh
+++ testapps.sh
@@ -26,21 +26,50 @@ tests=(
 )
 
 count=0
+child_pid=""
+interrupted=0
+
+on_int() {
+    interrupted=1
+    if [ -n "$child_pid" ]; then
+        kill -INT "$child_pid" 2>/dev/null
+    fi
+}
+
+trap on_int INT
 
 for test in "${tests[@]}"; do
+    if [ "$interrupted" -ne 0 ]; then
+        break
+    fi
+
     count=$((count + 1))
     printf '>> Executing: ./run.pl %q' "$test"
     for arg in "$@"; do
         printf ' %q' "$arg"
     done
     printf '\n'
-    ./run.pl "$test" "$@"
+    ./run.pl "$test" "$@" &
+    child_pid=$!
+    wait "$child_pid"
     rc=$?
+    child_pid=""
+
+    if [ "$interrupted" -ne 0 ]; then
+        break
+    fi
 
     if [ "$rc" -ne 0 ]; then
         echo "Program '$test' failed on test $count: exit code $rc" >&2
         exit "$rc"
     fi
 done
+
+trap - INT
+
+if [ "$interrupted" -ne 0 ]; then
+    echo "Interrupted by SIGINT." >&2
+    exit 130
+fi
 
 echo "$count tests ran and they were all successful."
