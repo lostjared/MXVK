2fe1aba2f8c38523fcd661259eb4e19f0477f976
Jared Bruni <jaredbruni@gmail.com>
2026-06-20 16:04:44 -0700

    #endif fix

--- examples/compute_shader/main.cpp
+++ examples/compute_shader/main.cpp
@@ -1317,9 +1317,7 @@ class ComputeWindow : public mxvk::VK_Window {
                     img.image,
                     img.memory);
             }
-        } else
-#else
-        {
+        } else {
             createImage(
                 static_cast<uint32_t>(texWidth),
                 static_cast<uint32_t>(texHeight),
@@ -1331,6 +1329,18 @@ class ComputeWindow : public mxvk::VK_Window {
                 img.image,
                 img.memory);
         }
+#else
+        createImage(
+            static_cast<uint32_t>(texWidth),
+            static_cast<uint32_t>(texHeight),
+            VK_FORMAT_R8G8B8A8_UNORM,
+            VK_IMAGE_TILING_OPTIMAL,
+            VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
+                VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+            img.image,
+            img.memory);
+#endif
         img.view = createImageView(img.image, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_ASPECT_COLOR_BIT);
 
         transitionImageLayout(
