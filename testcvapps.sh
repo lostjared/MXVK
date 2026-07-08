5642430a18b9118e1558541b4ad113fa664aa6ba
Jared Bruni <jaredbruni@gmail.com>
2026-07-08 06:37:44 -0700

    updated script

--- testcvapps.sh
+++ testcvapps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./run.pl opencv_example --camera $1
-./run.pl opencv_model --camera $1 --filename ./models/cube.mxmod.z
+./run.pl opencv_model --camera $1 --filename `pwd`/models/obj/cube.obj
 ./run.pl compute_shader --camera $1
-
 
