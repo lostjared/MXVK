0d3082f688b5ee06f173d48b892d44a285f320ff
Jared Bruni <jaredbruni@gmail.com>
2026-09-03 02:24:12 -0700

    project example CMakeLists.txt

---
+++ project_example/main.cpp
@@ -0,0 +1,47 @@
+#include "mxvk/argz.hpp"
+#include "mxvk/mxvk.hpp"
+#include "mxvk/mxvk_exception.hpp"
+#include <SDL3/SDL.h>
+#include <format>
+#include <iostream>
+
+namespace skeleton {
+    class SkeletonWindow : public mxvk::VK_Window {
+      public:
+        SkeletonWindow(int width, int height, bool full, bool enable_vsync) : mxvk::VK_Window(" -[ MXVK Skeleton ] - ", width, height, full, MXVK_VALIDATION, enable_vsync) {
+            std::cout << "skeleton: started example.\n";
+        }
+        void event(SDL_Event &e) override {
+            switch (e.type) {
+            case SDL_EVENT_KEY_DOWN:
+                if (e.key.key == SDLK_ESCAPE) {
+                    exit();
+                    return;
+                }
+                break;
+            case SDL_EVENT_QUIT:
+                exit();
+                return;
+            }
+        }
+        void proc() override {
+
+        }
+    };
+} // namespace skeleton
+
+int main(int argc, char **argv) {
+    try {
+        Arguments args = proc_args(argc, argv);
+        skeleton::SkeletonWindow window(args.width, args.height, args.fullscreen, args.enable_vsync);
+        window.loop();
+    } catch (mxvk::Exception &e) {
+        std::cerr << std::format("mxvk: Exception: {}\n", e.text());
+        return EXIT_FAILURE;
+    } catch (ArgException<std::string> &e) {
+        std::cerr << std::format("mxvk: Argument Exception: {}\n", e.text());
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
