5565c079ac942ea8398a58ba41527d27f9cfb6cf
Jared Bruni <jaredbruni@gmail.com>
2026-09-12 14:53:03 -0700

    forgot to set default argument for SavePNG function

--- mxvk/include/mxvk/mxvk_png.hpp
+++ mxvk/include/mxvk/mxvk_png.hpp
@@ -25,7 +25,7 @@ namespace mxvk {
      * @param filename Destination file path.
      * @return @c true on success, @c false on failure.
      */
-    bool SavePNG(SDL_Texture *texture, SDL_Renderer *renderer, const char *filename, int compression_level);
+    bool SavePNG(SDL_Texture *texture, SDL_Renderer *renderer, const char *filename, int compression_level = 6);
 
     /**
      * @brief Save raw data to a file.
