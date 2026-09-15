383b4bd2403e69b970ec2349e68789eece14649e
Jared Bruni <jaredbruni@gmail.com>
2026-09-15 08:55:38 -0700

    removed old scripts

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cmake -S . -B build $@ && cmake --build ./build -j$(nproc) && sudo cmake --install ./build
-
--- testcvapps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./run.pl opencv_example --camera $1
-./run.pl opencv_model --camera $1 --filename `pwd`/models/obj/cube.obj
-./run.pl compute_shader --camera $1
-
